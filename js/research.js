f87e990747926f518104de0bee755bf4749d7fe6
Wendy <wendygoodwin@mac.com>
2016-06-11 23:08:56 -0700

    javascript page layout

--- js/research.js
+++ js/research.js
@@ -1,13 +1,10 @@
 
-//1.listen for click on list-group-item-heading//
-//2.toggle the article in that list-group//
-$(document).ready() {
-  var articleExpand = {
-    var article = document.getElementByClassName('list-group');
-    var articleTitle = document.getElementsByClassName('list-group-item-heading');
-    var articleBody = document.getElementByClassName('list-group-item-text');
-    var expand = articleTitle.click()articleBody.toggle('slow');
-  }
-  
+console.log('testingJs')
 
-}
+$(document).ready(function() {
+  $('div.list-group').click(function(){
+    $('div.list-group-item-text', this).toggle('slow');
+    console.log(this);
+
+  })
+});
