a020660803f2de88a27c6586b4223acb7c48482c
Wendy <wendygoodwin@mac.com>
2016-06-13 17:48:50 -0700

    content for research page almost done

--- js/research.js
+++ js/research.js
@@ -2,9 +2,29 @@
 console.log('testingJs')
 
 $(document).ready(function() {
+
+  //This opens the different articles//
   $('div.list-group').click(function(){
     $('div.list-group-item-text', this).toggle('slow');
     console.log(this);
+  });
 
+  $('.specialHeader').click(function(){
+    $('.specialText').toggle('slow');
   })
+
+  $('.closeArticle').click(function(){
+    $('.specialText').toggle('slow');
+  })
+
+  $('.secondSpecialHeader').click(function(){
+    $('.secondSpecialText').toggle('slow');
+  })
+
+  $('.secondCloseArticle').click(function(){
+    $('.secondSpecialText').toggle('slow');
+  })
+
+
+
 });
