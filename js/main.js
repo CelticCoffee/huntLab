d4fc8c760599fccf25f44b141bcf31cf8452dbe2
Wendy <wendygoodwin@mac.com>
2016-06-25 18:59:10 -0700

    index semifinal

--- js/main.js
+++ js/main.js
@@ -15,4 +15,70 @@ $(document).ready(function() {
    cssEase: 'linear'
  });
 
+ $('#objectivesTab').click(function(event){
+     event.preventDefault();
+   $('.objectivesArticle').toggle('slow');
+ })
+
+
+
+$('#experimentsTab').click(function(event){
+    event.preventDefault();
+  $('.modelExperiments').toggle('slow');
+})
+
+$('#ooDesignTab').click(function(event){
+  event.preventDefault();
+  $('.objectODesign').toggle('slow');
+})
+
+$('#occamTab').click(function(event){
+  event.preventDefault();
+  $('.occamsRazorArticle').toggle('slow');
+})
+
+$('#iterativeTab').click(function(event){
+  event.preventDefault();
+  $('.iterativeArticle').toggle('slow');
+})
+
+
+//To go to the additional articles on modeling//
+  $('#moreModelingInfo').click(function(){
+    $('#mainBreadCrumb').toggle('slow');
+    $('#modelingTarget').toggle('slow');
+    $('.expandedModelingText').toggle('slow');
+    $(this).toggle();
+  })
+
+//When user wants to return to main home screen//
+ $('.modelingSubMenu').click(function(){
+   $('.expandedModelingText').toggle('slow');
+   $('#moreModelingInfo').toggle('slow');
+   $('#modelingTarget').toggle('slow');
+   $('#mainBreadCrumb').toggle('slow');
+ })
+
+  //The bottom close button (another way to go back to main home)//
+  $('.closeText').click(function(){
+    $('.expandedModelingText').toggle('slow');
+    $('#moreModelingInfo').toggle('slow');
+    $('#modelingTarget').toggle('slow');
+    $('#mainBreadCrumb').toggle('slow');
+  })
+
+  //jumbotron style//
+  $('.questionSlides').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+     autoplay: false,
+     autoplaySpeed: 6000,
+     dots: true
+  });
+
+  
+
+
+
+
 });
